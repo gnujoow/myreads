fc46e2603e34e5b9e65d171056384fc14ef2893d
WOOJUNG <gnujoow.kim@gmail.com>
2017-09-09 17:13:37 +0900

    add eslint rules
    add eslint rules which follows project spec

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+// import * as BooksAPI from './BooksAPI';
+import './App.css';
 
 class BooksApp extends React.Component {
   state = {
@@ -190,8 +190,8 @@ class BooksApp extends React.Component {
           </div>
         )}
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
